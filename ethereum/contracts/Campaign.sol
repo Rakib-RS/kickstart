fd0d5f1500ebb21a9d925006c71b83816542d3bc
rakib-rs <rakib.rscse@gmail.com>
2021-06-17 11:51:40 +0600

    function modifiers

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -1,10 +1,30 @@
 pragma solidity ^0.4.17;
 
 contract Campaign {
+    struct Request{
+        string description;
+        uint value;
+       address receipt;
+       bool complete;
+    }
+
+    Request[] public request;
     address public manager;
-    function Campaign()public {
+    uint public minimumContribution;
+    address[] public approvers;
+
+    modifier restrictedO(){
+        require(msg.sender == manager)'
+        _;
+    }
+    function Campaign(contribution)public {
         manager = msg.sender;
+        minimumContribution = contribution
         
+    }
+    function contribute() payable {
+        require(msg.value > minimumContribution);
+        approvers.push(msg.sender);
     }
     
 }
