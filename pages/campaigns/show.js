07f3514f9cfa43997535d67644c97d126f767a30
rakib-rs <rakib.rscse@gmail.com>
2021-07-01 09:56:00 +0600

    view campaign routing

---
+++ pages/campaigns/show.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class CampaignShow extends Component {
+    render() {
+        return (
+            <div>
+                show campaigns
+            </div>
+        )
+    }
+}
+
+export  default CampaignShow;
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,47 @@
-import React, { Component } from 'react'
-import factory from '../ethereum/factory';
-import Layout from '../components/Layout';
-import {Card,Button} from 'semantic-ui-react'
+import React, { Component } from "react";
+import factory from "../ethereum/factory";
+import Layout from "../components/Layout";
+import { Card, Button } from "semantic-ui-react";
+import { Link } from "../routes";
 
 class index extends Component {
-    static async getInitialProps(){
-        const campaigns = await factory.methods.getDeployedCampaign().call();
-        console.log(campaigns);
-        return {campaigns};
-        
-    }
-    renderCampaign () {
-        const items = this.props.campaigns.map( address =>{
-           return{
-               header:address,
-               description:<a>view campaign</a>,
-               fluid:true
-           };
-
-        })
-        return <Card.Group items={items}/>;
-    }
-    render() {
-        return (
-           <Layout>
-               <h3>Open Campaigns</h3>
-               <Button content="create campaign" icon="add circle" primary floated="right"/>
-               {this.renderCampaign()}
-           </Layout>
-        )
-    }
+  static async getInitialProps() {
+    const campaigns = await factory.methods.getDeployedCampaign().call();
+    console.log(campaigns);
+    return { campaigns };
+  }
+  renderCampaign() {
+    const items = this.props.campaigns.map((address) => {
+      return {
+        header: address,
+        description: (
+          <Link route={`/campaigns/${address}`}>
+            <a>view campaign</a>
+          </Link>
+        ),
+        fluid: true,
+      };
+    });
+    return <Card.Group items={items} />;
+  }
+  render() {
+    return (
+      <Layout>
+        <h3>Open Campaigns</h3>
+        <Link route="/campaigns/new">
+          <a>
+            <Button
+              content="create campaign"
+              icon="add circle"
+              primary
+              floated="right"
+            />
+          </a>
+        </Link>
+        {this.renderCampaign()}
+      </Layout>
+    );
+  }
 }
 
-export default  index;
+export default index;
